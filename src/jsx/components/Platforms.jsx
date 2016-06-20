af36d746a7f0d08702aa4c5c118c35cc6fa3e18a
Horia Coman <horia141@gmail.com>
2016-06-20 13:01:09 +0300

    New stuff in platforms.

--- src/jsx/components/Platforms.jsx
+++ src/jsx/components/Platforms.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContentWrapper from './Layout/ContentWrapper';
+import { Grid, Row, Col, Panel, Button } from 'react-bootstrap';
 
 class Platforms extends React.Component {
 
@@ -10,6 +11,47 @@ class Platforms extends React.Component {
           Platforms
           <small>Platforms settings</small>
 	</div>
+        <Row>
+          <Col sm={ 12 }>
+            <div className="panel panel-default">
+              <div className="panel-heading">Website</div>
+              <div className="panel-body">
+                <p>Details about site</p>
+              </div>
+              <div className="panel-footer">
+                <Button bsStyle="primary">Save</Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={ 12 }>
+            <div className="panel panel-default">
+              <div className="panel-heading">Call Center</div>
+              <div className="panel-body">
+                <p>Details about site</p>
+              </div>
+              <div className="panel-footer">
+                <Button bsStyle="primary">Save</Button>
+	      </div>
+            </div>          
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={ 12 }>
+            <div className="panel panel-default">
+              <div className="panel-heading">Email Center</div>
+              <div className="panel-body">
+                <p>Details about site</p>
+              </div>
+              <div className="panel-footer">
+                <Button bsStyle="primary">Save</Button>
+              </div>
+            </div>          
+          </Col>
+        </Row>
       </ContentWrapper>
     );
   }
