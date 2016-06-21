be2b1635080505aeb288f690dd03358e35677e37
Horia Coman <horia141@gmail.com>
2016-06-22 00:57:13 +0300

    Finalish form for hours range.

--- src/jsx/components/General.jsx
+++ src/jsx/components/General.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContentWrapper from './Layout/ContentWrapper';
 import { Grid, Row, Col, Panel, Button, ButtonGroup, Input, FormControl } from 'react-bootstrap';
+import HoursRange from './HoursRange';
 
 class General extends React.Component {
 
@@ -10,75 +11,46 @@ class General extends React.Component {
       name: 'Horia\'s Kitchen',
       desc: 'Fancy french restaurant',
       address: 'Sector 2, Bucharest',
-      monFriNonStop: false,
-      monFriStart: '07:00 AM',
-      monFriEnd: '10:00 PM',
+      monFriHours: {
+        nonStop: false,
+	start: '07:00 AM',
+	end: '10:00 PM'
+      },
+      satHours: {
+        nonStop: false,
+	start: '07:00 AM',
+	end: '10:00 PM'
+      },
+      sunHours: {
+        nonStop: false,
+	start: '07:00 AM',
+	end: '10:00 PM'
+      }
     };
   }
 
-  componentDidMount() {
-    $('#dtp-mon-fri-start').datetimepicker({format: 'hh:mm A'});
-    $('#dtp-mon-fri-start').on('dp.change', this.handleMonFriStartChange.bind(this));
-    $('#dtp-mon-fri-stop').datetimepicker({format: 'LT'});
-    $('#dtp-mon-fri-stop').on('dp.change', this.handleMonFriStopChange.bind(this));
-    $('#dtp-sat-start').datetimepicker({format: 'LT'});
-    $('#dtp-sat-stop').datetimepicker({format: 'LT'});
-    $('#dtp-sun-start').datetimepicker({format: 'LT'});
-    $('#dtp-sun-stop').datetimepicker({format: 'LT'});
-  }
-
   handleNameChange(e) {
-    this.setState({
-      name: e.target.value
-    });
+    this.setState({name: e.target.value});
   }
 
   handleDescChange(e) {
-    this.setState({
-      desc: e.target.value
-    });
+    this.setState({desc: e.target.value});
   }
 
   handleAddressChange(e) {
-    this.setState({
-      address: e.target.value
-    });
+    this.setState({address: e.target.value});
   }
 
-  handleMonFriNonStopChange(e) {
-    this.setState({
-      monFriNonStop: e.target.checked
-    });
+  handleMonFriHoursChange(e) {
+    this.setState({monFriHours: e});
   }
 
-  handleMonFriStartChange(e) {
-    var monFriStart;
-    if (e.hasOwnProperty('date')) {
-      // Set through datetimepicker.
-      monFriStart = e.date.format('hh:mm A');
-    } else {
-      // Set through manually editing the field.
-      monFriStart = e.target.value;
-    }
-
-    this.setState({
-      monFriStart: monFriStart
-    });
+  handleSatHoursChange(e) {
+    this.setState({satHours: e});
   }
 
-  handleMonFriEndChange(e) {
-    var monFriEnd;
-    if (e.hasOwnProperty('date')) {
-      // Set through datetimepicker.
-      monFriEnd = e.date.format('hh:mm A');
-    } else {
-      // Set through manually editing the field.
-      monFriEnd = e.target.value;
-    }
-
-    this.setState({
-      monFriEnd: monFriEnd
-    });
+  handleSunHoursChange(e) {
+    this.setState({sunHours: e});
   }
 
   render() {
@@ -155,107 +127,23 @@ class General extends React.Component {
               <div className="panel-heading">Opening Hours</div>
               <div className="panel-body">
 	        <form className="form-horizontal">
-
 		  { /* Monday to Friday opening times */ }
-                  <div className="form-group">
-                    <label className="col-lg-2 col-xs-12 control-label">Mon-Fri</label>
-                    <Col lg={ 2 } xs={ 4 }>
-                      <div className="checkbox c-checkbox">
-                        <label>
-                          <input
-			    standalone
-			    type="checkbox"
-			    checked={ this.state.monFriNonStop }
-			    onChange={ this.handleMonFriNonStopChange.bind(this) } />
-                          <em className="fa fa-check"></em>Non-stop
-			</label>
-                      </div>
-                    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-mon-fri-start" className="input-group date">
-                        <input
-			  type="text"
-			  disabled={ this.state.monFriNonStop }
-			  value={ this.state.monFriStart }
-			  onChange={ this.handleMonFriStartChange.bind(this) }
-			  className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-mon-fri-stop" className="input-group date">
-                        <input
-			  type="text"
-			  disabled={ this.state.monFriNonStop }
-			  value={ this.state.monFriEnd }
-			  onChange={ this.handleMonFriEndChange.bind(this) }
-			  className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		  </div>
+		  <HoursRange
+		    xid='mon-fri'
+		    label={ 'Mon-Fri' }
+		    onChange={ this.handleMonFriHoursChange.bind(this) } />
 
 		  { /* Saturday opening times */ }
-                  <div className="form-group">
-                    <label className="col-lg-2 col-xs-12 control-label">Saturday</label>
-                    <Col lg={ 2 } xs={ 4 }>
-                      <div className="checkbox c-checkbox">
-                        <label>
-                          <input standalone type="checkbox" />
-                          <em className="fa fa-check"></em>Non-stop
-			</label>
-                      </div>
-                    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-sat-start" className="input-group date">
-                        <input type="text" className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-sat-stop" className="input-group date">
-                        <input type="text" className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		  </div>
+		  <HoursRange
+		    xid='sat'
+		    label={ 'Saturday' }
+		    onChange={ this.handleSatHoursChange.bind(this) } />
 
 		  { /* Sunday opening times */ }
-                  <div className="form-group">
-                    <label className="col-lg-2 col-xs-12 control-label">Sunday</label>
-                    <Col lg={ 2 } xs={ 4 }>
-                      <div className="checkbox c-checkbox">
-                        <label>
-                          <input standalone type="checkbox" />
-                          <em className="fa fa-check"></em>Non-stop
-			</label>
-                      </div>
-                    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-sun-start" className="input-group date">
-                        <input type="text" className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		    <Col lg={ 4 } xs={ 4 }>
-                      <div id="dtp-sun-stop" className="input-group date">
-                        <input type="text" className="form-control" />
-                        <span className="input-group-addon">
-                          <span className="fa fa-clock-o"></span>
-                        </span>
-                      </div>
-		    </Col>
-		  </div>
+		  <HoursRange
+		    xid='sun'
+		    label={ 'Sunday' }
+		    onChange={ this.handleSunHoursChange.bind(this) } />
 		</form>
 	      </div>
               <div className="panel-footer">
