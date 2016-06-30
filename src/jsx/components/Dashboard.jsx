da54519b1d23c0c84f57ba7cd335d318ba620503
Horia Coman <horia141@gmail.com>
2016-07-01 00:21:29 +0300

    Add charts to dashboard.

--- src/jsx/components/Dashboard.jsx
+++ src/jsx/components/Dashboard.jsx
@@ -1,8 +1,94 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import ContentWrapper from './Layout/ContentWrapper';
 import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
+var datav3 = [{
+  "label": "Orders",
+  "color": "#1ba3cd",
+  "data": [
+	   ["1", 38],
+	   ["2", 40],
+	   ["3", 42],
+	   ["4", 48],
+	   ["5", 50],
+	   ["6", 70],
+	   ["7", 145],
+	   ["8", 70],
+	   ["9", 59],
+	   ["10", 48],
+	   ["11", 38],
+	   ["12", 29],
+	   ["13", 30],
+	   ["14", 22],
+	   ["15", 28]
+	   ]
+    }, {
+        "label": "Items",
+        "color": "#3a3f51",
+        "data": [
+		 ["1", 16],
+		 ["2", 18],
+		 ["3", 17],
+		 ["4", 16],
+		 ["5", 30],
+		 ["6", 110],
+		 ["7", 19],
+		 ["8", 18],
+		 ["9", 110],
+		 ["10", 19],
+		 ["11", 16],
+		 ["12", 10],
+		 ["13", 20],
+		 ["14", 10],
+		 ["15", 20]
+		 ]
+    }];
+
+var options = {
+    series: {
+	lines: {
+	    show: true,
+	},
+	points: {
+	    show: true,
+	    radius: 4
+	}
+    },
+    grid: {
+	borderColor: '#eee',
+	borderWidth: 1,
+	hoverable: true,
+	backgroundColor: '#fcfcfc'
+    },
+    tooltip: true,
+    tooltipOpts: {
+	content: function(label, x, y) {
+	    return x + ' : ' + y;
+	}
+    },
+    xaxis: {
+	tickColor: '#fcfcfc',
+	mode: 'categories'
+    },
+    yaxis: {
+	min: 0,
+	max: 150, // optional: use it for a clear represetation
+	tickColor: '#eee',
+	//position: 'right' or 'left',
+	tickFormatter: function(v) {
+	    return v /* + ' visitors'*/ ;
+	}
+    },
+    shadowSize: 0
+};
+
 class Dashboard extends React.Component {
+
+  componentDidMount() {
+    var chartNode = ReactDom.findDOMNode(this.refs.chartSpline);
+    $.plot(chartNode, datav3, options);
+  }
 
   render() {
     return (
@@ -67,6 +153,21 @@ class Dashboard extends React.Component {
                     <div className="text-uppercase">Revenue</div>
                   </Col>
 		</Row>
+              </div>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col lg={ 12 }>
+              <div id="panelChart9" className="panel panel-default">
+                <div className="panel-heading">
+                  <div className="panel-title">Orders And Items</div>
+                </div>
+                <div collapse="panelChart9" className="panel-wrapper">
+                  <div className="panel-body">
+                    <div ref="chartSpline" className="chart-splinev3 flot-chart"></div>
+                  </div>
+                </div>
               </div>
             </Col>
           </Row>
