08dc8fa5a1d995e61dd78e48bd5cc8220a0cec5d
Horia Coman <horia141@gmail.com>
2016-06-30 10:14:57 +0300

    Complete the sections area of the menu.

--- src/jsx/components/Menu.jsx
+++ src/jsx/components/Menu.jsx
@@ -25,16 +25,90 @@ class Menu extends React.Component {
 	      <div className="panel-body">
                 <div className="list-group">
                   <div className="media p mt0 list-group-item">
-                    <span className="pull-left">
-                      <img src="img/mood01.jpg" className="media-object img-circle thumb32" />
-                    </span>
-                    <span className="media-body">
-                      <span className="media-heading">
-                        <strong>Soups</strong>
-                        <br/>
-                        <small className="text-muted">Our nice selection of soups</small>
-                      </span>
-                    </span>
+		    <span className="close">&times;</span>
+		    <Row>
+		      <Col md={ 1 }>
+                        <span className="pull-left">
+                          <img src="img/mood01.jpg" className="media-object img-circle thumb32" />
+                        </span>
+		      </Col>
+		      <Col md={ 3 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Soups</strong>
+                            <br/>
+                            <small className="text-muted">Our nice selection of soups</small>
+                          </span>
+                        </span>
+		      </Col>
+		      <Col md={ 6 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Tomato Soup, Mushroom Soup</strong>
+                            <br/>
+                            <small className="text-muted">And 5 other soups</small>
+                          </span>
+                        </span>
+		      </Col>
+		    </Row>
+                  </div>
+
+                  <div className="media p mt0 list-group-item">
+		    <span className="close">&times;</span>
+		    <Row>
+		      <Col md={ 1 }>
+                        <span className="pull-left">
+                          <img src="img/mood02.jpg" className="media-object img-circle thumb32" />
+                        </span>
+		      </Col>
+		      <Col md={ 3 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Pastas</strong>
+                            <br/>
+                            <small className="text-muted">A bunch of pastas</small>
+                          </span>
+                        </span>
+		      </Col>
+		      <Col md={ 6 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Carbonara, Bolognese</strong>
+                            <br/>
+                            <small className="text-muted">And 6 other pastas</small>
+                          </span>
+                        </span>
+		      </Col>
+		    </Row>
+                  </div>
+
+                  <div className="media p mt0 list-group-item">
+		    <span className="close">&times;</span>
+		    <Row>
+		      <Col md={ 1 }>
+                        <span className="pull-left">
+                          <img src="img/mood03.jpg" className="media-object img-circle thumb32" />
+                        </span>
+		      </Col>
+		      <Col md={ 3 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Drinks</strong>
+                            <br/>
+                            <small className="text-muted">To ease things down</small>
+                          </span>
+                        </span>
+		      </Col>
+		      <Col md={ 6 }>
+                        <span className="media-body">
+                          <span className="media-heading">
+                            <strong>Beck's, White Wine</strong>
+                            <br/>
+                            <small className="text-muted">And 10 other drinks</small>
+                          </span>
+                        </span>
+		      </Col>
+		    </Row>
                   </div>
                 </div>
 	      </div>
