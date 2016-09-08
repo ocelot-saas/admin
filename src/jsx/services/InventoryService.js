4f2b0cc59b15d099731a6a213f1ad533cce35964
Horia Coman <horia141@gmail.com>
2016-09-08 09:44:57 +0300

    Extend service layer with platforms operations.

--- src/jsx/services/InventoryService.js
+++ src/jsx/services/InventoryService.js
@@ -77,4 +77,115 @@ export class InventoryService {
             }
         );
     }
+
+    getPlatformsWebsiteFromService() {
+	var accessToken = this._identityService.getAccessToken();
+	return new Promise(
+	    (resolve, reject) => {
+		$.get({
+		    url: `http://${this._inventoryServiceDomain}/org/platforms/website`,
+		    dataType: 'json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsWebsiteResponse) => {
+                    resolve(platformsWebsiteResponse.platformsWebsite);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
+
+    updatePlatformsWebsiteFromService(updatePlatformsWebsiteRequest) {
+        var accessToken = this._identityService.getAccessToken();
+        return new Promise(
+            (resolve, reject) => {
+                $.ajax({
+                    type: 'PUT',
+                    url: `http://${this._inventoryServiceDomain}/org/platforms/website`,
+                    dataType: 'json',
+                    data: JSON.stringify(updatePlatformsWebsiteRequest),
+                    contentType: 'application/json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsWebsiteResponse) => {
+                    resolve(platformsWebsiteResponse.platformsWebsite);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
+
+    getPlatformsCallcenterFromService() {
+	var accessToken = this._identityService.getAccessToken();
+	return new Promise(
+	    (resolve, reject) => {
+		$.get({
+		    url: `http://${this._inventoryServiceDomain}/org/platforms/callcenter`,
+		    dataType: 'json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsCallcenterResponse) => {
+                    resolve(platformsCallcenterResponse.platformsCallcenter);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
+
+    updatePlatformsCallcenterFromService(updatePlatformsCallcenterRequest) {
+        var accessToken = this._identityService.getAccessToken();
+        return new Promise(
+            (resolve, reject) => {
+                $.ajax({
+                    type: 'PUT',
+                    url: `http://${this._inventoryServiceDomain}/org/platforms/callcenter`,
+                    dataType: 'json',
+                    data: JSON.stringify(updatePlatformsCallcenterRequest),
+                    contentType: 'application/json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsCallcenterResponse) => {
+                    resolve(platformsCallcenterResponse.platformsCallcenter);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
+
+    getPlatformsEmailcenterFromService() {
+	var accessToken = this._identityService.getAccessToken();
+	return new Promise(
+	    (resolve, reject) => {
+		$.get({
+		    url: `http://${this._inventoryServiceDomain}/org/platforms/emailcenter`,
+		    dataType: 'json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsEmailcenterResponse) => {
+                    resolve(platformsEmailcenterResponse.platformsEmailcenter);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
+
+    updatePlatformsEmailcenterFromService(updatePlatformsEmailcenterRequest) {
+        var accessToken = this._identityService.getAccessToken();
+        return new Promise(
+            (resolve, reject) => {
+                $.ajax({
+                    type: 'PUT',
+                    url: `http://${this._inventoryServiceDomain}/org/platforms/emailcenter`,
+                    dataType: 'json',
+                    data: JSON.stringify(updatePlatformsEmailcenterRequest),
+                    contentType: 'application/json',
+                    headers: {'Authorization': `Bearer ${accessToken}`}
+                }).done((platformsEmailcenterResponse) => {
+                    resolve(platformsEmailcenterResponse.platformsEmailcenter);
+                }).fail((xhr) => {
+                    reject(xhr.status);
+                });
+            }
+        );
+    }
 }
