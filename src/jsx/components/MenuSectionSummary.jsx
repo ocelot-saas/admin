361e254436bd464ef70d03e928f486d9f75b9f4d
Horia Coman <horia141@gmail.com>
2016-09-22 23:32:07 +0300

    Factored out the summaries to new classes.

---
+++ src/jsx/components/MenuSectionSummary.jsx
@@ -0,0 +1,33 @@
+import { Row, Col } from 'react-bootstrap';
+
+
+export default function MenuSectionSummary(props) {
+    return (
+        <div className="media p mt0 list-group-item" onClick={ props.onClick }>
+            <span className="close">&times;</span>
+            <span className="pull-left">
+                <img src="/img/mood01.jpg" className="media-object img-circle thumb32" />
+            </span>
+            <Row>
+                <Col md={ 3 }>
+                    <span className="media-body">
+                        <span className="media-heading">
+                            <strong>{ props.menuSection.name }</strong>
+                            <br/>
+                            <small className="text-muted">{ props.menuSection.description }</small>
+                        </span>
+                    </span>
+                </Col>
+                <Col md={ 6 }>
+                    <span className="media-body">
+                        <span className="media-heading">
+                            <strong>Tomato Soup, Mushroom Soup</strong>
+                            <br/>
+                            <small className="text-muted">And 5 other soups</small>
+                        </span>
+                    </span>
+                </Col>
+            </Row>
+        </div>
+    );
+}
